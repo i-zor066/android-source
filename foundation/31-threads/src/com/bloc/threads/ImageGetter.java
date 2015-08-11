dd91f6b2fa007a19bfd53637204694e81b11abc7
i-zor066 <igor.zor@gmail.com>
2015-08-11 13:02:35 +0200

    Assignment 31 completed

--- foundation/31-threads/src/com/bloc/threads/ImageGetter.java
+++ foundation/31-threads/src/com/bloc/threads/ImageGetter.java
@@ -1,4 +1,8 @@
 package com.bloc.threads;
+import java.net.URL;
+import java.io.*;
+import javax.imageio.*;
+import java.awt.image.BufferedImage;
 
 public class ImageGetter extends Thread {
 
@@ -12,12 +16,18 @@ public class ImageGetter extends Thread {
 	 * @param openWhenCompleted if `true`, the image will be opened
 	 *		  as soon it is downloaded, `false` otherwise
 	 */
+    private String mURL;
+	private Boolean mOpenWhenCompleted;
 	/************************************************
  	 *	ASSIGNMENT:
  	 *	Implement this constructor for ImageGetter.
  	 *	The variables passed into it must impact the `run()` method's
  	 *	behavior as described above.
 	/************************************************/
+	public ImageGetter(String url, boolean openWhenCompleted) {
+		mURL = url;
+		mOpenWhenCompleted = openWhenCompleted;
+	}    
 
 	@Override
 	public void run() {
@@ -26,5 +36,26 @@ public class ImageGetter extends Thread {
  		 *	Perform the work found in `Main.java` in this
  		 *	Thread instead.
 		/************************************************/
+        try {
+			File existingImage = new File("google_logo.png");
+			if (existingImage.exists()) {
+				existingImage.delete();
+			}
+			URL url = new URL("https://www.google.com/images/srpr/logo11w.png");
+			BufferedImage bufferedImage = ImageIO.read(url);
+			File outputfile = new File("google_logo.png");
+			ImageIO.write(bufferedImage, "png", outputfile);
+			if ("/".equals(System.getProperties().getProperty("file.separator"))) {
+				Runtime.getRuntime().exec("open google_logo.png");
+			} else {
+				Runtime.getRuntime().exec("google_logo.png");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 }
